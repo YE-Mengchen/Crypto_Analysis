8910ed0a55f75378e99051f241d0f443384bcecd
Mengchen Ye <914864251@qq.com>
2023-12-11 17:19:00 +1100

    Change from use config to .env which is safer

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+require('dotenv').config();
 
 const connectDB = async() => {
     try{
-        await mongoose.connect(db);
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log("MongoDB Connected!");
     } catch(err){
         console.error(err.message);
